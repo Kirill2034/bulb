a5bac0fe12242602261fead7214bd8c72dfed598
Kirill2034 <dum15.00@mail.ru>
2021-07-25 16:17:02 +0400

    delete comments

--- src/Bulb.js
+++ src/Bulb.js
@@ -3,12 +3,6 @@ import './Bulb.css';
 const classNames = require('classnames');
 
 function Bulb(props) {
-    // const classes = ['bulb']
-
-    // if(props.isActive) {
-    //     classes.push('active')
-    // }
-
     return <div className={classNames('bulb', { 'active': props.isActive })}>
         {props.isActive ? <p>Не трать энергию!</p> : <p>Включи меня</p>}
     </div>
